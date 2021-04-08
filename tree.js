4642929129c0e198ece661571ef3fc8c87cf68c6
Manpreet03 <69601469+Manpreet03@users.noreply.github.com>
2021-04-08 18:09:11 +0530

    Update tree.js

--- tree.js
+++ tree.js
@@ -1,23 +1,29 @@
-class Tree {
-    constructor(x, y, width, height) {
-        var options = {
-            isStatic: true
-        }
+class Tree
+{
+	constructor(x,y)
+	{
+		this.x=x;
+		this.y=y;
+		this.treeWidth=450;
+		this.treeHeight=600;
+		this.treeThickness=10;
+		
+		this.image=loadImage("tree.png")
+		this.bottomBody=Bodies.rectangle(this.x, this.y, this.treeWidth, this.treeThickness, {isStatic:true})
+		World.add(world, this.bottomBody)
 
-        this.body = Bodies.rectangle(x, y, width, height, options); 
-        this.width = width; 
-        this.height = height; 
-        this.image = loadImage("Plucking mangoes/tree.png"); 
+	}
+	
+	display()
+	{
+			var posBottom=this.bottomBody.position;
+			push()
+			translate(posBottom.x, posBottom.y+10);
+			fill(255)
+			imageMode(CENTER);
+			image(this.image, 0,-this.treeHeight/2,this.treeWidth, this.treeHeight)
+			pop()
+			
+	}
 
-        World.add(world, this.body)
-    }
-
-    display() {
-        var pos = this.body.position; 
-
-        push(); 
-        imageMode(CENTER); 
-        image(this.image, 0, 0); 
-        pop()
-    }
 }
